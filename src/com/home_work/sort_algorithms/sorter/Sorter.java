eb10969663433d92b641a5ad6b5c3f592aad100e
VChorney <v56chorney95@gmail.com>
2019-10-04 14:01:30 +0300

    seperate swapping in one private method

--- src/com/home_work/sort_algorithms/sorter/Sorter.java
+++ src/com/home_work/sort_algorithms/sorter/Sorter.java
@@ -2,13 +2,10 @@ package com.home_work.sort_algorithms.sorter;
 
 public class Sorter {
     public static int[] bubbleSort(int[] array) {
-        int temp;
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = 0; j < array.length - i - 1; j++)
                 if (array[j] > array[j + 1]) {
-                    temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
+                    swap(array, j, j + 1);
                 }
         }
         return array;
@@ -21,15 +18,13 @@ public class Sorter {
             heapify(array, n, i);
 
         for (int i = n - 1; i >= 0; i--) {
-            int temp = array[0];
-            array[0] = array[i];
-            array[i] = temp;
+            swap(array,0,i);
 
             heapify(array, i, 0);
         }
         return array;
     }
-    
+
     private static void heapify(int array[], int n, int i) {
         int largest = i;
         int l = 2 * i + 1;
@@ -42,11 +37,15 @@ public class Sorter {
             largest = r;
 
         if (largest != i) {
-            int swap = array[i];
-            array[i] = array[largest];
-            array[largest] = swap;
+            swap(array,i,largest);
 
             heapify(array, n, largest);
         }
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
